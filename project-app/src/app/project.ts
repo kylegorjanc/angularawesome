9a67e7d522c3f03f81acba9ad1876e9641621f75
Kyle Gorjanc <kylegorjanc@gmail.com>
2017-03-23 23:18:45 -0400

    initialized project

---
+++ project-app/src/app/project.ts
@@ -0,0 +1,15 @@
+export class Project {
+  project_title: { type: String, required: true, unique: true };
+  category: { type: String, default: 'design', required: true };
+  link: { type: String, required: true };
+  thumbnail_url: { type: String, required: true };
+  blurb: { type: String, required: true };
+  content: { type: String, required: true };
+
+
+    constructor(values: Object = {}) {
+    Object.assign(this, values);
+  }
+}
+
+})
